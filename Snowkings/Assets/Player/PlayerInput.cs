3fd91b398955e8c799b1dab269f34e93f74eed67
Anthony Bogli <anthonybogli1@gmail.com>
2015-05-13 12:15:43 +1000

    Added Scripts and prefabs
    Created A Increase Speed, Increase Size as well as a Decriment Size and
    Speed Script

--- Snowkings/Assets/Player/PlayerInput.cs
+++ Snowkings/Assets/Player/PlayerInput.cs
@@ -6,6 +6,13 @@ public class PlayerInput : MonoBehaviour {
 	public float thrust; //Just using it for direction
 	public Rigidbody rb; //Used for getting the rigidbody component
 	public SphereCollider bounds; 
+	Vector3 LastFrame;
+	Vector3 Force;
+	public Vector3 Currentsize;
+
+	KeyCode leftKey;
+	KeyCode rightKey;
+
 
 
 	// Use this for initialization
@@ -14,14 +21,26 @@ public class PlayerInput : MonoBehaviour {
 		bounds = GetComponent<SphereCollider> ();
 		thrust = 10.0f;
 
+		Currentsize = new Vector3 (0, 0, 0);
+
+		LastFrame = new Vector3 (0, 0, 0);
+		Force = new Vector3 (0, 0, 0);
+
+		leftKey = KeyCode.A;
+		rightKey = KeyCode.D;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		Force =  this.transform.position - LastFrame;
+
 
 		if (Input.GetKey (KeyCode.W)) {
 			transform.localScale += new Vector3(.10F,.10F,.10F);
+			Currentsize += new Vector3(.10F,.10F,.10F);
+
 			bounds.radius += .00005f;
 
 		}
@@ -30,12 +49,12 @@ public class PlayerInput : MonoBehaviour {
 		if (Input.GetKey (KeyCode.S)) {
 			//TODO: Make the ball lose speed/velocity
 			//e.g Make the ball slow down
-			rb.AddForce(new Vector3(0,-1,0) * thrust);
-
+			rb.AddForce(Force * 100);
+			//rb.velocity += Force;
 
 		}
 
-		if (Input.GetKey (KeyCode.A)) {
+		if (Input.GetKey (leftKey)) {
 			//TODO: Add func here
 			//Shift the ball to the left
 			//transform.position.x += 1;
@@ -43,20 +62,46 @@ public class PlayerInput : MonoBehaviour {
 			rb.AddForce(new Vector3(1,0,0) * thrust);
 		}
 
-		if (Input.GetKey (KeyCode.D)) {
+		if (Input.GetKey (rightKey)) {
 			//TODO: shift the ball to the right
 			//transform.position = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
 			rb.AddForce(new Vector3(-1,0,0) * thrust);
-
-
-
 		}
 
 
-
-
-	
-
-
+		LastFrame = transform.position;
 	}
+
+	public void IncrimentSize(Vector3 Scale)
+	{
+		transform.localScale += Scale;
+	}
+
+	public void IncrimentSpeed(float Multiplier)
+	{
+		rb.velocity += Force * Multiplier;
+	}
+
+	public void IncrimentMass(float mass)
+	{
+		rb.mass += mass;
+	}
+
+	public void ReduceSize(Vector3 SizeReduction)
+	{
+		transform.localScale -= SizeReduction;
+	}
+
+	public void ReduceSpeed(float Multiplier)
+	{
+		rb.velocity -= -Force * Multiplier;
+	}
+
+	public void InitaliseKeys(KeyCode left, KeyCode right)
+	{
+		leftKey = left;
+		rightKey = right;
+	}
+
+
 }
---
+++ Snowkings/Assets/Player/SpeedBoost.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedBoost : MonoBehaviour {
+
+	//Using this to incriment the players movement speed
+	//Just attatch this to objects and done! :)
+	GameObject obj;
+	PlayerInput player;
+
+	// Use this for initialization
+	void Start () {
+		obj = GameObject.Find ("Player1");
+		player = obj.GetComponent<PlayerInput> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player") {
+			Debug.Log("Incrimenting speed");
+			obj = other.gameObject;
+			player = obj.GetComponent<PlayerInput> ();
+			player.IncrimentSpeed(100);
+		}
+
+
+
+	}
+}
